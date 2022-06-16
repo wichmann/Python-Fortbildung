32ff73e2e1501cc7144cc7702555b655ca1a6505
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2022-06-16 17:32:34 +0200

    Make URL shortener exercise ready.

---
+++ url-shortener_aufgabe.py
@@ -0,0 +1,63 @@
+
+"""
+Es soll ein Kurz-URL-Dienst [1] entwickelt werden. Dazu ist eine Web-App nötig,
+die beim Aufruf eines HTTP-Endpunktes (z.B. /redirect/xxx) automatisch zu einer
+vorher gespeicherten URL weiterleitet. Verschiedene Kürzel führen zu unter-
+schiedlichen Internet-Adressen. Die Kurz-URL kann dann statt der kompletten,
+langen URL weitergegeben werden.
+
+Mögliche Erweiterungen:
+Neben dem Endpunkt zur Weiterleitung gibt es einen Server-Pfad, über den neue
+Weiterleitungen eingerichtet werden können. Zur Verwaltung des Servers wäre
+eine Übersichtsseite sinnvoll, auf der alle Kurz-URLs aufgelistet werden.
+
+Hilfen:
+ * Handbuch des Pakets Flask [2]
+ * Einführender Artikel zu Flask [3]
+
+Bibliotheken:
+ * Flask - Leichtgewichtiges Web Application Framework
+    pip install flask
+
+Quellen:
+[1] https://de.wikipedia.org/wiki/Kurz-URL-Dienst
+[2] https://flask.palletsprojects.com/en/2.1.x/
+[3] https://www.heise.de/ratgeber/Python-Framework-fuer-Webentwicklung-Einfacher-Einstieg-mit-Flask-6340642.html
+
+"""
+
+
+from flask import Flask
+
+
+# initialisiere HTTP-Server
+app = Flask(__name__)
+
+
+# erzeuge Vorlage für alle Seiten
+html_template = """<html>
+                     <head>
+                        <title>{title}</title>
+                     </head>
+                     <body>
+                       <h1>{heading}</h1>
+                       {content}
+                     </body>
+                   </html>"""
+
+
+@app.route('/', methods=['GET'])
+def index():
+    text = 'Generator für Kurz-URLs'
+    content = 'Herzlich Willkommen...'
+    return html_template.format(title=text, heading=text, content=content)
+
+
+@app.route('/redirect/<shorturl>')
+def redirect_to_original_url(shorturl):
+    pass
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
--- url-shortener_lösung.py
+++ url-shortener_lösung.py
@@ -1,12 +1,28 @@
 
 """
-Eine Webserver, der Kurz-URLs erstellt und beim Aufruf zur Original-Seite weiterleitet.
+Es soll ein Kurz-URL-Dienst [1] entwickelt werden. Dazu ist eine Web-App nötig,
+die beim Aufruf eines HTTP-Endpunktes (z.B. /redirect/xxx) automatisch zu einer
+vorher gespeicherten URL weiterleitet. Verschiedene Kürzel führen zu unter-
+schiedlichen Internet-Adressen. Die Kurz-URL kann dann statt der kompletten,
+langen URL weitergegeben werden.
+
+Mögliche Erweiterungen:
+Neben dem Endpunkt zur Weiterleitung gibt es einen Server-Pfad, über den neue
+Weiterleitungen eingerichtet werden können. Zur Verwaltung des Servers wäre
+eine Übersichtsseite sinnvoll, auf der alle Kurz-URLs aufgelistet werden.
+
+Hilfen:
+ * Handbuch des Pakets Flask [2]
+ * Einführender Artikel zu Flask [3]
 
 Bibliotheken:
- * pip install flask
+ * Flask - Leichtgewichtiges Web Application Framework
+    pip install flask
 
 Quellen:
- * https://flask.palletsprojects.com/en/2.0.x/
+[1] https://de.wikipedia.org/wiki/Kurz-URL-Dienst
+[2] https://flask.palletsprojects.com/en/2.1.x/
+[3] https://www.heise.de/ratgeber/Python-Framework-fuer-Webentwicklung-Einfacher-Einstieg-mit-Flask-6340642.html
 
 """
 
@@ -66,7 +82,8 @@ def add_short_url():
         short_url = hashlib.md5(original_url.encode('utf8')).hexdigest()[:10]
         saved_urls[short_url] = original_url
         print(saved_urls)
-        content = 'Kurz-URL für <a href="{0}">{0}</a> lautet <a href="{1}">{1}</a>'.format(original_url, url_for('redirect_to_original_url', _external=True, shorturl=short_url))
+        content = 'Kurz-URL für <a href="{0}">{0}</a> lautet <a href="{1}">{1}</a>'.format(
+            original_url, url_for('redirect_to_original_url', _external=True, shorturl=short_url))
         return html_template.format(title=text, heading=text, content=content)
 
 
@@ -82,10 +99,11 @@ def redirect_to_original_url(shorturl):
 @app.route('/list')
 def list_short_urls():
     title = 'Liste aller Kurz-URLs'
-    content = '<br>'.join(['{} -> {}'.format(k, saved_urls[k]) for k in saved_urls.keys()])
+    content = '<br>'.join(['{} -> {}'.format(k, saved_urls[k])
+                          for k in saved_urls.keys()])
     return html_template.format(title=title, heading=title, content=content)
 
 
 if __name__ == '__main__':
-    #app.debug = True
-    app.run(host= '0.0.0.0', port=5000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
